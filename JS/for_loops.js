bd7319e205c3a1c83486bb4241e04107725a099e
Matthew Kauff <matthew.kauff@live.com>
2022-02-09 15:36:03 -0500

    add while loop practice and for loop

---
+++ JS/for_loops.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/*
+function showMultiplicationTable(num) {
+    for (let i = num; i < 100; i*i) {
+        console.log();
+    }
+}
+
+showMultiplicationTable(1);
+*/
+
+let num = 9;
+while (num >= 8 && num <= 50){
+    console.log(num++);
+}
+console.log("The loop has ended");
