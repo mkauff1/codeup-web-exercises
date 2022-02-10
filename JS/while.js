eda111ac709a7539b9b6dfac3d72398df8c8ccd2
Matthew Kauff <matthew.kauff@live.com>
2022-02-09 23:46:29 -0500

    Add cones exercise for do-while loop

--- JS/while.js
+++ JS/while.js
@@ -1,2 +1,36 @@
 "use strict";
+/*
+let num = 1;
+
+while (num <= 66000) {
+    console.log((num));
+    num *= 2;
+}
+*/
+
+/*
+let i = 1
+while (i < 17) {
+    console.log(Math.pow(2, i));
+    i++;
+}
+ */
+
+
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+do {
+    let conesSold = Math.floor(Math.random() * 5) + 1;
+    console.log(allCones);
+    console.log(" There are " + allCones + " cones left to sell!");
+    if (conesSold > allCones){
+        console.log("I cannot sell you " + conesSold + " cones, because I only have " + allCones);
+        continue;
+    } else {
+        console.log(conesSold + " cones sold");
+        allCones -= conesSold;
+    }
+
+} while (allCones > 0);
+console.log("Yay, there are no cones left!");
 
