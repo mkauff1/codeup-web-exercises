f2156bf63716276c81072cfa53393eb77290bbfd
Matthew Kauff <matthew.r.kauff@gmail.com>
2022-03-11 12:33:37 -0500

    add jquery exercise

---
+++ js/jquery_exercises.js
@@ -0,0 +1,16 @@
+"use strict";
+
+//$(document).ready(function() {
+//    alert( 'The DOM has finished loading!' );
+//});
+
+//let hobo = $('#hello').html();
+//alert(hobo);
+
+$('.codeup').css('border', '1px solid #F00');
+
+$('.font-wee').css('font-size', '20px');
+
+
+
+$('h1, .font-wee, p').css('background-color', '#FF0');
