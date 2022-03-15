102da47bcb11160501e88b5c3e93b5cdb464d235
Matthew Kauff <matthew.r.kauff@gmail.com>
2022-03-15 12:12:15 -0400

    add traverse jquery

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -3,12 +3,28 @@
 $(document).ready(function(){
     $("dd").addClass("invisible");
 
-    $("dd").click(function(){
-        $(".invisible").css("visibility", "hidden");
-        $(this).toggleClass(".invisible")
+    $("#btn1").click(function(){
+        //$(".invisible").css("visibility", "hidden");
+        $("dd").toggleClass("invisible");
     });
 
-    $("dd").hover(function() {
+    $("dd").click(function() {
         $(this).toggleClass( "highlight" );
     });
+
+    $("#btn2").click(function (){
+        $("ul").each(function(){
+            $(this).children().last().toggleClass("highlight");
+        });
+    });
+
+    $("h3").addClass("bold-font");
+    $("h3").click(function(){
+        $(this).next().children().toggleClass("bold-font");
+    });
+
+    $("li").click(function(){
+        $(this).parent().children().first().css("font", "blue");
+    });
 });
+
