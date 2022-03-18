3ae6db444ae76a38cafcd61fa9ef38ee5bb34eb2
Matthew Kauff <matthew.r.kauff@gmail.com>
2022-03-18 16:12:32 -0400

    add integrated weather api and mapbox

--- js/weathermap_mapbox_utils.js
+++ js/weathermap_mapbox_utils.js
@@ -5,8 +5,8 @@ mapboxgl.accessToken = MAP_KEY;
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
-    //center: [-79.4512, 43.6568],
-    zoom: 13
+    center: [-83.17823878255246, 42.1417394047964],
+    zoom: 10
 });
 
 // Add the control to the map.
