68b0b5aa2c06557acdfa85af5893877ba2089f0f
Matthew Kauff <matthew.r.kauff@gmail.com>
2022-03-21 16:43:50 -0400

    add geocoder and test map

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,5 +1,5 @@
-let cityLat = document.getElementById("searchLat").value;
-let cityLon = document.getElementById("searchLon").value;
+var cityLat = document.getElementById("searchLat").value;
+var cityLon = document.getElementById("searchLon").value;
 function getWeatherData() {
 
     let weatherStatsUrl = ("https://api.openweathermap.org/data/2.5/onecall?lat=" + cityLat + "&lon=" + cityLon + "&units=imperial&appid=" + OWM_KEY);
@@ -12,43 +12,46 @@ function getWeatherData() {
 
             let mainContainer = document.getElementById("container");
 
-            //for (let weath of data.daily) {
-            for (let i = 0; i < 5; i++) {
+            for (let i = 0; i < 5; i++) {   //for (let weath of data.daily) {
                 let weath = data.daily[i];
                 let d = document.createElement("div"); // Single day
                 d.classList.add("card", "col-4", "m-1");
                 d.style.width = "12rem";
-                let dateOfWeather = document.createElement("div");
-                dateOfWeather.classList.add("card-header");
 
-                dateOfWeather.innerHTML = "Time: " + weath.dt; //Want to switch date from unit to UTC
-
+                // Get Date and make it UTC
+                let unixDate = weath.dt;
                 function formatDate(unixDate) {
                     return new Date(unixDate * 1000).toISOString().split('T')[0];
                 }
+                let dateOfWeather = document.createElement("div");
+                dateOfWeather.classList.add("card-header");
+                dateOfWeather.innerHTML = "Date: " + formatDate(unixDate);
 
+                // Create Div with added attributes below
                 function appendingAttribute(parent, content) {
                     let div = document.createElement("div");
                     div.innerHTML = content;
                     parent.appendChild(div);
                 }
 
+                // Show Date Attribute
                 d.appendChild(dateOfWeather);
 
+                // Add Temp Attribute + Description TODO: Fix Image source
                 appendingAttribute(d, "Temp: " + weath.temp.min + "/" + weath.temp.max + " F" + "<hr>");
                 for (let dayWeather of weath.weather) {
-                    //let iconWeath = `http://openweathermap.org/img/w/[icon].png`;
-                    appendingAttribute(d, "Description: " + dayWeather.description + " " + dayWeather.icon);
+                    //const icon1 = `http://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}@2x.png`;
+                    appendingAttribute(d, "Description: " + dayWeather.description + " " + "<img src=`https://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}@2x.png`>");
                 }
-
+                // Add other Attributes
                 appendingAttribute(d, "Humidity: " + weath.humidity);
                 appendingAttribute(d, "Wind: " + weath.wind_speed + "mph");
                 appendingAttribute(d, "Pressure: " + weath.pressure);
 
                 mainContainer.appendChild(d);
-
             }
-
+            // TODO: Need to remove location divs
+            //mainContainer.removeChild();
         });
 }
 document.getElementById("searchLatLon").addEventListener('click', getWeatherData)
--- js/weathermap_mapbox_utils.js
+++ js/weathermap_mapbox_utils.js
@@ -1,87 +1,79 @@
 "use strict";
 
 mapboxgl.accessToken = MAP_KEY;
+let startingLat = 29.4252;
+let startingLon = -98.4916;
 
-let geocoder;
-let marker;
-let popup;
+let map = initMap(startingLon, startingLat);
+let geocoder = initGeoCoder();
+let marker = createMarker(startingLon, startingLat);
+let popup = createPopup(startingLon, startingLat);
 
-/**
- * Invoke the initial functions to kick off our application
- * **/
-init();
+let currentLocation = [startingLon, startingLat];
+marker = createMarker(currentLocation);
+
+// add geocoder search box to map
+map.addControl(
+    geocoder
+);
+
 setGeocoderEventListener();
 
+marker.setPopup(popup);
 
-/**Define a function to instantiate the map and geocoder objects and bind them together**/
-function init() {
-
-    /*Make the mapbox map object, set to map variable declared above*/
-    const map = new mapboxgl.Map({
+// function to create map
+function initMap(lon, lat) {
+    return new mapboxgl.Map({
         container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11',
-        center: [-83.17823878255246, 42.1417394047964],
-        zoom: 10
+        style: 'mapbox://styles/mapbox/dark-v10',
+        zoom: 10,
+        center: [lon, lat]
     });
-
-    /*Make the geocoder object, set to the geocoder variable declared above*/
-    geocoder = new MapboxGeocoder({
-        accessToken: MAP_KEY,
-        mapboxgl: mapboxgl,
-        marker: true
-    });
-
-    /*Add the geocoder variable value to the map as a control (form input)*/
-    map.addControl(geocoder);
 }
 
-/**
- * Utility function to get a new Marker object whenever invoked
- * param coordinates: number array containing the lng lat of the location
- * **/
-function getMarker(coordinates) {
+// function to create marker
+function createMarker(lon, lat) {
     return new mapboxgl.Marker()
-        .setLngLat(coordinates)
+        .setLngLat([lon, lat])
         .addTo(map);
 }
 
-
-/**
- * Utility function to get a new Popup object whenever invoked
- * param description: string represented details of the location
- * param coordinates: number array containing the lng lat of the location
- * **/
-function getPopup(description, coordinates) {
+// function to create popup
+function createPopup(lon, lat) {
     return new mapboxgl.Popup()
-        .setLngLat(coordinates)
-        .setHTML(`<p>${description}</p>`)
-        .addTo(map);
+        .setLngLat([lon, lat])
+        .setHTML("<p>Codeup Rocks!</p>")
 }
 
+// function to create geocoder
+function initGeoCoder() {
+    return new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl
+    })
+}
 
-/**
- * Encapsulates code to listen for the geocoder to return a result and allows us to get new Marker and Popup objects
- * **/
+// geocode with built in geocoder just getting the center map will automatically fly to entered location
 function setGeocoderEventListener() {
-    geocoder.on("result", function (e) {
-        /*We need to ensure marker/popup variables hoisted at the top actual *have* a value
-        * Otherwise, calling a remove() method on a non-existent object will result in a runtime error
-        * */
-        console.log(e.result);
+    geocoder.on('result', function (e) {
+        console.log(e.result)
+
         if (marker) {
             marker.remove();
         }
         if (popup) {
             popup.remove();
         }
-
-        /*Finally, set the hoisted marker/popup variables to new respective objects*/
-        //marker = getMarker(e.result.geometry.coordinates);
-        //popup = getPopup(e.result.place_name, e.result.geometry.coordinates);
         cityLat = e.result.center[1];
-
         cityLon = e.result.center[0];
         console.log(cityLat, cityLon);
+
         getWeatherData();
     });
 }
+
+map.on('click', function (e) {
+    console.log(e);
+    marker = createMarker([e.lngLat.lng, e.lngLat.lat]);
+    getWeatherData(e.lngLat.lat, e.lngLat.lng);
+})
