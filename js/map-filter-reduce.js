9637acf8ec4e8c150e76e86e8835c7472e244511
Matthew Kauff <matthew.r.kauff@gmail.com>
2022-03-23 10:36:09 -0400

    add map filter reduce complete

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -41,3 +41,52 @@ const users = [
 
 let multipleLanguages = users.filter(l => l.languages.length > 2);
 console.log(multipleLanguages);
+
+let usersWithThreeOrMoreLanguages = users.filter(function(user){
+    return user.languages.length >= 3;
+});
+usersWithThreeOrMoreLanguages.forEach(function(user){
+    console.log(user.languages);
+})
+
+let userEmail = users.map(m => m.email);
+console.log(userEmail);
+
+let totalYearsExp = users.reduce((years, user) => {
+    return years + user.yearsOfExperience;
+}, 0);
+console.log(totalYearsExp);
+
+let averageYearsExp = totalYearsExp / users.length;
+console.log(averageYearsExp);
+
+let longestEmail = users.reduce((longest, user) => {
+    if(user.email.length > longest.length){
+        longest = user.email;
+    }
+    return longest;
+}, '');
+console.log(longestEmail)
+
+let instructorUserNameList = users.reduce((userNames, user) => {
+    if(users.id < 3) {
+        return userNames += user.name + ", ";
+    } else {
+        return userNames += user.name + ".";
+    }
+}, 'Your instructors are: ');
+
+console.log(instructorUserNameList);
+
+//Bonus
+
+let languages = users.reduce((listOfLanguages, user) => {
+    user.languages.forEach(function (lang){
+        if(!listOfLanguages.includes(lang)){
+            listOfLanguages.push(lang);
+        }
+    })
+    return listOfLanguages;
+}, []);
+
+console.log(languages);
